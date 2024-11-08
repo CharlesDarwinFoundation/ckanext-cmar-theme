d12da079fff1cee2c826c38d2155a72856223ee9
Anna Dolma Alonso <dolma33@gmail.com>
2024-11-08 22:05:09 +0000

    Add Latest Documents section to Homepage

--- ckanext/cmar_theme/plugin.py
+++ ckanext/cmar_theme/plugin.py
@@ -2,11 +2,18 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckan.lib.plugins import DefaultTranslation
 
+def latest_datasets(count=9):
+    datasets = toolkit.get_action('package_search')(
+            {}, {'sort': 'metadata_created desc', 'rows': count,
+                 'fl': 'name,title,notes,metadata_created'}
+        )
 
+    return datasets['results']
 
 class CmarThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
     plugins.implements(plugins.ITranslation)
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
     
     # IConfigurer
     def update_config(self, config_):
@@ -15,3 +22,5 @@ class CmarThemePlugin(plugins.SingletonPlugin, DefaultTranslation):
         toolkit.add_resource("assets", "cmar_theme")
 
     
+    def get_helpers(self):
+        return {'cmar_theme_latest_datasets': latest_datasets}
